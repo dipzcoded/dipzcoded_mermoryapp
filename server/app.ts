d984d007124575f3897011c82628b2e4f348d741
dipzcoded <dipoakerele@gmail.com>
2021-03-07 02:03:08 +0100

    changed

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { config as dotenv } from "dotenv";
@@ -13,6 +13,10 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/api/posts", postRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello to Memory API");
+});
 
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.CONNECTION_URL!.replace(
