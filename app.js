c7793d2ca45b2fbf862c397ff575b06a905ea50d
dipzcoded <dipoakerele@gmail.com>
2021-04-02 01:15:44 +0100

    refactored

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const { config } = require("dotenv");
 
@@ -17,6 +18,14 @@ app.use(cors());
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 
+// displaying d front end
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.CONNECTION_URL.replace(
   "PASSWORD",
